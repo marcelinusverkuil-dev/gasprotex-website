ac62b2a788ac9ef56b44385586b1763b7df38f3b
Marcelinus Verkuil <marcelinus_verkuil@hotmail.com>
2026-04-10 14:44:43 +0200

    hero foto src gecorrigeerd naar hero-image.jpg

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -29,7 +29,7 @@ export default function HeroSection() {
 
       {/* Achtergrond foto */}
       <img
-        src="/hero-image-v2.jpg"
+        src="/hero-image.jpg"
         alt="GasProtex inspecteur bij industriele installatie"
         className="absolute inset-0 w-full h-full object-cover"
         style={{ filter: 'brightness(0.5)' }}
